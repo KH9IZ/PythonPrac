4299ce80fba63cf9d441b2ebce916978bbabe9c0
Knyazev Egor <knyazevegor01@mail.ru>
2021-10-27 20:39:08 +0300

    Task 20211021/3

---
+++ 20211021/3/prog.py
@@ -0,0 +1,16 @@
+from collections import Counter
+n = int(input())
+
+cntr = Counter()
+while s := input():
+    s = ''.join(c.lower() if c.isalpha() or c.isspace() else ' ' for c in s)
+    words_n = [word for word in s.split() if len(word) == n]
+    cntr.update(words_n)
+t = max(cntr.values(), default=0)
+l = []
+for el, cnt in cntr.most_common():
+    if cnt < t:
+        break
+    l.append(el)
+if l:
+    print(*sorted(l))
