385d79ac694f5299b1f4f638f19e1ac2bec0e625
Knyazev Egor <knyazevegor01@mail.ru>
2021-10-27 15:52:25 +0300

    tests for 20211021/1/2

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -8,9 +8,12 @@ while True:
         f = s[0][1:]
         v = s[1]
         e = ' '.join(s[2:])  # Возможны ошибкИ?
-        fs[f] = lambda x: eval(e, math.__dict__, {v: x})
+        fs[f] = (e, v)
     elif s.split()[0] == 'quit':
         print(len(fs) + 1)
+        break
     else:
         s = s.split()
-        print(fs[s[0]](eval(s[1])))
+        e = fs[s[0]][0]
+        v = fs[s[0]][1]
+        print(eval(e, math.__dict__, {v: eval(' '.join(s[1:]))}))
