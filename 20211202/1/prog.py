c9fad7a629392df52c03dd7e9b38221d0f3ef455
Knyazev Egor <knyazevegor01@mail.ru>
2021-12-05 17:43:02 +0300

    First task 20211202 done!

--- 20211202/1/prog.py
+++ 20211202/1/prog.py
@@ -1,10 +1,14 @@
 from functools import wraps
+
 class dump(type):
     def __init__(self, name, parents, ns):
         for f_name, f in filter(lambda x: callable(x[1]), ns.items()):
             @wraps(f)
-            def my_f(*args, f=f, f_name=f_name, **kwargs):
-                print(f"{f_name}: {args[1:]}, {kwargs}")
-                return f(*args, **kwargs)
+            def my_f(self, *args, f=f, f_name=f_name, **kwargs):
+                print(f"{f_name}: {args}, {kwargs}")
+                return f(self, *args, **kwargs)
             setattr(self, f_name, my_f)
         return super().__init__(name, parents, ns)
+
+import sys
+exec(sys.stdin.read())
