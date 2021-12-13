ce9f0ca628226e3986463c27cd249daf154e3338
Knyazev Egor <knyazevegor01@mail.ru>
2021-12-14 00:56:14 +0300

    Всё сломал

--- 20211213/1/prog.py
+++ 20211213/1/prog.py
@@ -2,43 +2,48 @@ import random
 import asyncio
 from collections import defaultdict
 
-L = list(range(16))
-random.shuffle(L)
+L = eval(input())
 LL = L.copy()
 
 
-async def merge(b0, b1, e1, n, el, er, myev):
-    e0 = b1
-    i = b0
-    b = b0
-    await el.wait()
-    await er.wait()
+async def merge(begin0, begin1, end1, l_ev, r_ev, my_ev):
+    await r_ev.wait()
+    await l_ev.wait()
 
-    while b0 < e0 and b1 < e1:
-        if L[b0] < L[b1]:
-            LL[i] = L[b0]
-            b0 += 1
+    end0 = begin1
+    i = begin0
+    begin = begin0
+
+    while begin0 < end0 and begin1 < end1:
+        if LL[begin0] < L[begin1]:
+            LL[i] = L[begin0]
+            begin0 += 1
         else:
-            LL[i] = L[b1]
-            b1 += 1
+            LL[i] = L[begin1]
+            begin1 += 1
         i += 1
-    await asyncio.sleep(0.1)
-    print("-> ",n)
-    LL[i:e1] = L[b0:e0] + L[b1:e1]
-    L[b:e1] = LL[b:e1]
-    myev.set()
+
+    await asyncio.sleep(0)
+
+    LL[i:end1] = L[begin0:end0] + L[begin1:end1]
+    L[begin:end1] = LL[begin:end1]
+    my_ev.set()
 
 async def joiner():
+
     events = defaultdict(asyncio.Event)
     tasks = []
-    n = 0
+
     for p in range(4):
         b = 2 ** (p+1)
         for i in range(0, len(L), b):
-            tasks.append(asyncio.create_task(merge(i, i+b // 2, i+b, n, events[f'{p}-{i}'], events[f'{p}-{i+b//2}'], events[f'{p+1}-{i}'])))
-            n += 1
+            tasks.append(asyncio.create_task(merge(i, i+b // 2, i+b, events[f'{p}-{i}'], events[f'{p}-{i+b//2}'], events[f'{p+1}-{i}'])))
+
     for i in range(16):
         events[f'0-{i}'].set()
+
     await asyncio.gather(*tasks)
+
+
 asyncio.run(joiner())
 print(L)
