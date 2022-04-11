1848b4ff10e923e4667d88590423c2f35cf4c73d
Knyazev Egor <knyazevegor01@mail.ru>
2022-04-11 13:38:59 +0300

    classwork 20220411

---
+++ 20220411/1/dia/__init__.py
@@ -0,0 +1,10 @@
+import gettext
+
+translation = gettext.translation("dia", "po", fallback=True)
+_ = translation.gettext
+ngettext = translation.ngettext
+
+def dialog():
+    while s := input(_("Input a string: ")):
+        n = len(s.split())
+        print(ngettext("{s} word entered", "{s} words entered", n).format(s=n))
