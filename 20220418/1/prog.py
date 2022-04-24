66ca1753f25f3baa5c9e97a54cd7511ef37133d8
Knyazev Egor <knyazevegor01@mail.ru>
2022-04-24 14:47:19 +0300

    20220418 solveSquare implementation

--- 20220418/1/prog.py
+++ 20220418/1/prog.py
@@ -1,2 +1,8 @@
-def solver():
-    pass
+def solveSquare(a, b, c):
+    discr = b**2 - 4*a*c
+    if discr >= 0:
+        x1 = (-b - discr**0.5) / (2 * a)
+        x2 = (-b + discr**0.5) / (2 * a)
+        return sorted(x1, x2)
+    else:
+        return None
